72d48c9fd0d6cf71a858e2064ef64210e380289c
Leonid Kuzin (a.k.a Dg_INC) <dg.inc.lcf@gmail.com>
2022-08-03 15:44:03 +0300

    WIP

--- core/app.php
+++ core/app.php
@@ -11,18 +11,11 @@ class app {
     function init(): void {
 
         Locale::init();
-        // главная страница вашсайт.рф
+
         Router::route('/', function () {
             print 'Домашняя станица';
         });
 
-        // маршрут будет срабатывать на адрес вашсайт.рф/blog/myrusakov/12091983
-        // и подобные
-        Router::route('blog/(\w+)/(\d+)', function ($category, $id) {
-            print $category . ':' . $id;
-        });
-
-        // запускаем маршрутизатор, передавая ему запрошенный адрес
         Router::execute($_SERVER['REQUEST_URI']);
     }
 }
